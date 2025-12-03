bac69f77b8dec852d80f0b619f099dbc0ce8df5d
jpierre-amar <amarjp13@gmail.com>
2025-12-03 17:19:14 +0100

    Ajout du composant ScrollToTop pour remonter en haut à chaque changement de page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { AppRouter } from './routes/Router';
+import { ScrollToTop } from './components/shared/ScrollToTop';
 
 function App() {
   return (
     <HelmetProvider>
       <BrowserRouter>
+        <ScrollToTop />
         <AppRouter />
       </BrowserRouter>
     </HelmetProvider>
